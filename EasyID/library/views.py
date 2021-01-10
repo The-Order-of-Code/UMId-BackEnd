57748120ba90832d9eb2ccbe4f34d566e6f9656f
Natas <36662762+GostoDeNatas@users.noreply.github.com>
2021-01-10 18:49:23 +0000

    Update reservation restrictions

--- EasyID/library/views.py
+++ EasyID/library/views.py
@@ -3,8 +3,12 @@ from .models import *
 from .serializers import *
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import generics, mixins
+from rest_framework import status
+from django.db.models import Q
+import datetime
 
 # Create your views here.
 
@@ -13,6 +17,11 @@ class RoomViewSet(viewsets.ModelViewSet):
 	serializer_class = RoomSerializer
 	authentication_classes = [SessionAuthentication, BasicAuthentication]
 	permission_classes = [IsAuthenticated, IsAdminUser]
+
+def reservationAvailable(roomId, startTime, endTime):
+	roomReservations = Reservation.objects.filter(room=roomId)
+	reservationBetween = roomReservations.filter(Q(start__lte=startTime) & Q(end__gte=startTime) | Q(start__lte=endTime) & Q(end__gte=endTime))
+	return startTime<endTime and len(reservationBetween)==0
 
 class ReservationViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
 												  mixins.CreateModelMixin,
@@ -29,3 +38,12 @@ class ReservationViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
 				return Reservation.objects.all()
 			else:
 				return Reservation.objects.all().filter(user=self.request.user)
+
+	def create(self, request, *args,**kwargs):
+		room = self.request.data["room"]
+		start = self.request.data["start"]
+		end = self.request.data["end"]
+		if reservationAvailable(room, start, end):
+			return super(ReservationViewSet, self).create(request, *args,**kwargs)
+		else:
+			return Response("Reservation time not available", status=status.HTTP_400_BAD_REQUEST)
